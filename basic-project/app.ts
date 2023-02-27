0dfe23af8a801a6502f869200ab6c9eafb6b117a
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2023-02-27 15:20:48 +0100

    Add union types

--- basic-project/app.ts
+++ basic-project/app.ts
@@ -39,3 +39,19 @@ const person = {
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
 }
+
+function combine(input1: number | string, input2: number | string) {
+  let result
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2
+  } else {
+    result = input1.toString() + input2.toString()
+  }
+  return result
+}
+
+const combinedAges = combine(30, 26)
+console.log(combinedAges)
+
+const combinedNames = combine('Gale', 'Carter')
+console.log(combinedNames)
